a7ff6e2cfa87c6f5f113268b70ea3fbe575f1885
kazuya kawaguchi <kawakazu80@gmail.com>
2025-03-06 18:59:17 +0900

    fix: knip error

--- knip.config.ts
+++ knip.config.ts
@@ -1,6 +1,6 @@
 import type { KnipConfig } from 'knip'
 
 export default {
-  entry: ['src/index.ts', 'eslint.config.ts'],
+  entry: ['src/index.ts', 'eslint.config.ts', 'rolldown.config.ts'],
   ignoreDependencies: ['lint-staged']
 } satisfies KnipConfig
