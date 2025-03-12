a3c897afe9d52a069d8c2bb692aecd3ae6eb66f3
kazuya kawaguchi <kawakazu80@gmail.com>
2025-03-12 17:35:14 +0900

    fix: renderer exports fields (#21)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsdown'
 
 const config: ReturnType<typeof defineConfig> = defineConfig({
-  entry: ['src/index.ts', 'src/context.ts', 'src/renderer.ts'],
+  entry: ['src/index.ts', 'src/context.ts', 'src/renderer/index.ts'],
   outDir: 'lib',
   dts: true
 })
