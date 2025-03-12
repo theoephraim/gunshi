246e1a8d120143562fba2cd2fe327e9e0381eb8b
kazuya kawaguchi <kawakazu80@gmail.com>
2025-03-12 13:07:06 +0900

    fix: updat simple example

--- playground/simple/index.js
+++ playground/simple/index.js
@@ -2,8 +2,6 @@ import { cli } from 'gunshi'
 
 // Simple API example
 // This demonstrates how to run a command with the Simple API
-cli(process.argv.slice(2), ctx => {
+cli(process.argv.slice(2), () => {
   console.log('Hello from simple example!')
-  console.log('Command line arguments:', ctx.positionals)
-  console.log('Command values:', ctx.values)
 })
