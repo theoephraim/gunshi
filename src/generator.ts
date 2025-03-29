a8035a8e0c3385394e7aa0b5a2c7a8c2b676b543
kazuya kawaguchi <kawakazu80@gmail.com>
2025-03-30 02:31:34 +0900

    breaking: drop command runner on generate (#49)

--- src/generator.ts
+++ src/generator.ts
@@ -2,18 +2,18 @@ import { cli } from './cli.ts'
 import { create } from './utils.ts'
 
 import { ArgOptions } from 'args-tokens'
-import type { Command, CommandOptions, CommandRunner } from './types.ts'
+import type { Command, CommandOptions } from './types.ts'
 
 /**
  * Generate the command usage
  * @param command - usage generate command, if you want to generate the usage of the default command where there are target commands and sub-commands, specify `null`.
- * @param entry - A {@link Command | entry command} or an {@link CommandRunner | inline command runner}
+ * @param entry - A {@link Command | entry command}
  * @param opts - A {@link CommandOptions | command options}
  * @returns A rendered usage
  */
 export async function generate<Options extends ArgOptions = ArgOptions>(
   command: string | null,
-  entry: Command<Options> | CommandRunner<Options>,
+  entry: Command<Options>,
   opts: CommandOptions<Options> = {}
 ): Promise<string> {
   const args = ['-h']
