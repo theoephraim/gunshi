d97f726806ff83ea0a0949b5fef498f33435baf1
kazuya kawaguchi <kawakazu80@gmail.com>
2025-04-11 17:24:49 +0900

    release: v0.14.1

--- playground/simple/index.js
+++ playground/simple/index.js
@@ -2,7 +2,17 @@ import { cli } from 'gunshi'
 
 // Simple API example
 // This demonstrates how to run a command with the Simple API
-await cli(process.argv.slice(2), ctx => {
-  console.log('Hello from simple example!')
-  console.log('Passing from arguments as raw', ctx._)
+await cli(process.argv.slice(2), {
+  // name: 'run',
+  // options: {
+  //   foo: {
+  //     type: 'string',
+  //     description: 'Foo option',
+  //     default: 'bar'
+  //   }
+  // },
+  run: async ctx => {
+    console.log('Hello from simple example!')
+    console.log('Passing from arguments as raw', ctx._)
+  }
 })
