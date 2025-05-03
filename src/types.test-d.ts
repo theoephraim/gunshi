6369ab859d46a04b51006a9e667d72596219ae00
kazuya kawaguchi <kawakazu80@gmail.com>
2025-05-03 18:36:06 +0900

    fix: readonly string array error for enum option (#106)

--- src/types.test-d.ts
+++ src/types.test-d.ts
@@ -1,4 +1,6 @@
 import { expectTypeOf, test } from 'vitest'
+import { cli } from './cli.ts'
+import { define } from './definition.ts'
 
 import type { ArgOptions } from 'args-tokens'
 import type { KeyOfArgOptions } from './types.ts'
@@ -17,4 +19,22 @@ test('KeyOfArgOptions', () => {
     }
   } satisfies ArgOptions
   expectTypeOf<KeyOfArgOptions<typeof _options>>().toEqualTypeOf<'foo' | 'bar' | 'baz' | 'no-baz'>()
+})
+
+test('specified read only value as enum option default', async () => {
+  const choices = ['a', 'b'] as const
+
+  const command = define({
+    options: {
+      foo: {
+        type: 'enum',
+        choices
+      }
+    },
+    run: ctx => {
+      expectTypeOf(ctx.values.foo).toEqualTypeOf<'a' | 'b' | undefined>()
+    }
+  })
+
+  await cli(['--foo', 'a'], command)
 })
