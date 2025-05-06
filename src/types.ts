94ec2f022a34b3d0aa38bf5b85de33df215309c0
kazuya kawaguchi <kawakazu80@gmail.com>
2025-05-06 20:46:09 +0900

    docs: tweak api references

--- src/types.ts
+++ src/types.ts
@@ -15,11 +15,13 @@ type RemoveIndexSignature<T> = {
 
 /**
  * Remove index signature from object or record type.
+ * @internal
  */
 export type RemovedIndex<T> = RemoveIndexSignature<{
   [K in keyof T]: T[K]
 }>
 
+/** @internal */
 export type KeyOfArgs<A extends Args> =
   | keyof A
   | {
@@ -32,6 +34,7 @@ export type KeyOfArgs<A extends Args> =
 
 /**
  * Generate a namespaced key.
+ * @internal
  */
 export type GenerateNamespacedKey<
   Key extends string,
@@ -40,11 +43,13 @@ export type GenerateNamespacedKey<
 
 /**
  * Command i18n built-in arguments keys.
+ * @internal
  */
 export type CommandBuiltinArgsKeys = keyof (typeof import('./constants.ts'))['COMMON_ARGS']
 
 /**
  * Command i18n built-in resource keys.
+ * @internal
  */
 export type CommandBuiltinResourceKeys =
   (typeof import('./constants.ts'))['COMMAND_BUILTIN_RESOURCE_KEYS'][number]
@@ -52,6 +57,7 @@ export type CommandBuiltinResourceKeys =
 /**
  * Command i18n built-in keys.
  * The command i18n built-in keys are used to {@link CommandContext.translate | translate} function.
+ * @internal
  */
 export type CommandBuiltinKeys =
   | GenerateNamespacedKey<CommandBuiltinArgsKeys>
@@ -62,6 +68,7 @@ export type CommandBuiltinKeys =
 /**
  * Command i18n option keys.
  * The command i18n option keys are used to {@link CommandContext.translate | translate} function.
+ * @internal
  */
 export type CommandArgKeys<A extends Args> = GenerateNamespacedKey<
   KeyOfArgs<RemovedIndex<A>>,
