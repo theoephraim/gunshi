7f092e865d288914b76019efa0b4cbf544cf44ee
kazuya kawaguchi <kawakazu80@gmail.com>
2025-05-06 15:58:05 +0900

    fix: update knip config

--- knip.config.ts
+++ knip.config.ts
@@ -13,6 +13,6 @@ export default {
     'docs/.vitepress/config.ts',
     'docs/.vitepress/theme/index.ts'
   ],
-  ignore: ['playground/deno/main.ts', 'src/constants.ts', 'src/**.test-d.ts'],
-  ignoreDependencies: ['lint-staged', 'deno']
+  ignore: ['playground/deno/main.ts', 'src/constants.ts', 'src/**.test-d.ts', 'bench/**'],
+  ignoreDependencies: ['lint-staged', 'deno', 'gunshi019']
 } satisfies KnipConfig
